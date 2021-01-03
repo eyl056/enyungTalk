68f8c0ee7809a360fb3d1c32413b28a00c0b6411
Eun Young <eyeyl0811@naver.com>
2021-01-03 19:03:17 +0900

    initial commit - create firebase project

---
+++ enyungTalk/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  enyungTalk
+//
+//  Created by EunYoung on 2020/12/30.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    var box = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
